e1145793aa89595367dac1db8d3ef331976fc83e
eniolaomotee <eniolaomotomi@gmail.com>
2025-02-14 12:46:35 +0100

    fix: issues with tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
 from fastapi.testclient import TestClient
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from main import app
 
 client = TestClient(app, base_url="http://test/api/v1")
